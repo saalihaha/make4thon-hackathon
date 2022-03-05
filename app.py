96aa22101c041c3558bb430d1ae23d3d2a33c546
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-03-05 21:16:15 +0530

    example

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = os.path.join(os.getcwd(),"images")
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
+token = ""
 
 
 
 @app.route('/',methods=["GET","POST"])
 def home():
     if request.method=="POST":
-        print("hello")
+       
         files = request.files
         image = files.get('file')
         
@@ -26,22 +26,27 @@ def home():
             filename = secure_filename(image.filename)
             image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             image_path = os.path.join(UPLOAD_FOLDER,filename)
-            print(run_classifier(image_path))
+            mood = run_classifier(image_path)
 
     return render_template('index.html')
 
 
 @app.route('/music',methods=["GET","POST"])
 def music():
-    pagetitle = "Music"
-    return render_template('musicplayer.html')
+    pagetitle = "Music",
+    name = "swapnil"
+    hackathon = "make4thon"
+    link = "htpps://dasd"
+    context = {
+        "pagetitle":pagetitle,
+        "name":name,
+        "hackathon":hackathon
+    }
+    return render_template('musicplayer.html',context=context)
 
 
 if __name__=='__main__':
     app.run(debug=True)
 
-'''
-if __name__=='__main__':
-    app.run('0.0.0.0',debug=True,ssl_context="adhoc")
-'''
+
 
